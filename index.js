f5b81815d9b8068a6adb48778ef5d90ba408e187
Igor Lima <igorlimagn@gmail.com>
2022-07-08 23:23:16 -0300

    @igorgll [Task: Creating select profile for Kenai Movies]

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Olá Mundo!')
+function time() {
+    today = new Date();
+    h = today.getHours();
+    m = today.getMinutes();
+
+    if (h < 12) {
+        document.getElementById('time').innerHTML= h + ':' + m + ' AM';
+        setTimeout('time()', 500);
+    }else {
+        document.getElementById('time').innerHTML= h + ':' + m + ' PM';
+        setTimeout('time()', 500);
+    }
+}
