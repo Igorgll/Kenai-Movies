c362381b5b962c3a2d891ac92db45c92071ec8b3
Igor Lima <igorlimagn@gmail.com>
2022-07-13 17:32:43 -0300

    @igorgll [Task: Js functions to set main movie]

---
+++ README.md
@@ -0,0 +1,30 @@
+<!-- ## Figma Project: https://www.figma.com/file/Yb9IBH56g7T1hdIyZ3BMNO/Desafios---Codel%C3%A2ndia?node-id=40282%3A715 -->
+
+<!-- ## Deploy: https://igorgll-kenai-movies.netlify.app/  -->
+
+<!-- ![Imgur](https://i.imgur.com/xG3s93M.png) -->
+
+<!-- ![Imgur](https://i.imgur.com/Z7Jzvmp.png) -->
+
+<h1 align="center">
+  Desafio Codelândia - Kenai Movies
+</h1>
+
+## 💻 Projeto
+
+Desafio Codelândia **Kenai Movies** usando _HTML_, _CSS_ e _JavaScript_. O conteúdo da aplicação é gerado dinamicamente através da (API) da [TMDB](https://www.themoviedb.org/documentation/api). Com essa api é possível carregar informações como título, descrição, capa, foto de background e nota de avaliação dos filmes selecionados.
+
+Layout do desafio no figma: [Kenai Movies](https://www.figma.com/file/AfcZjKNRxDWOSSI0vrnrls?node-id=0%3A105).
+
+Deploy do projeto: [Kenai](https://igorgll-kenai-movies.netlify.app/)
+
+## 🚀 Tecnologias
+
+- HTML
+- CSS
+- JavaScript
+- [API TMDB](https://www.themoviedb.org/documentation/api)
+
+![Imgur](https://i.imgur.com/xG3s93M.png)
+
+![Imgur](https://i.imgur.com/Z7Jzvmp.png)
--- index.js
+++ index.js
@@ -1,70 +1,106 @@
 // TIMESTAMP
 function time() {
-    today = new Date();
-    h = today.getHours();
-    m = today.getMinutes();
+  today = new Date();
+  h = today.getHours();
+  m = today.getMinutes();
 
-    if (h < 12) {
-        document.getElementById('time').innerHTML= h + ':' + m + ' AM';
-        setTimeout('time()', 500);
-    }else {
-        document.getElementById('time').innerHTML= h + ':' + m + ' PM';
-        setTimeout('time()', 500);
-    }
+  if (h < 12) {
+    document.getElementById("time").innerHTML = h + ":" + m + " AM";
+    setTimeout("time()", 500);
+  } else {
+    document.getElementById("time").innerHTML = h + ":" + m + " PM";
+    setTimeout("time()", 500);
+  }
 }
 
-// MOVIES 
-const API_KEY = '03c4e3dc470296959d6bf68804146538'
-const API_LANGUAGE = 'pt-br'
+// MOVIES
+const API_KEY = "03c4e3dc470296959d6bf68804146538";
+const API_LANGUAGE = "pt-br";
 
-const LIST_MOVIES = ['tt1877830', 'tt10872600', 'tt1211837', 'tt7286456','tt0816692' ,'tt0137523', 'tt1853728', 'tt1375666']
+const LIST_MOVIES = [
+  "tt1877830",
+  "tt7286456",
+  "tt0137523",
+  "tt1853728",
+  "tt4633694",
+  "tt0454921",
+  "tt2980516",
+  "tt2084970",
+  "tt4857264",
+  "tt10298810",
+  "tt1464335",
+  "tt2463208",
+  "tt7991608",
+  "tt11286314",
+  "tt9620288",
+];
 
 function get_url_movie(movieId) {
-    return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`
+  return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=${API_LANGUAGE}`;
 }
 
-fetch(get_url_movie(LIST_MOVIES[0]))
-.then(response  => response.json())
-.then(data => {
-    console.log(data)
-    
-    const app = document.getElementById('app')
-
-    const movie_title = document.querySelector('.movie h2')
-    const sinopse = document.querySelector('.movie #description')
-    const category = document.querySelector('.movie span')
-
-    const runtime = document.querySelector('.rating__time p')
-    
-    movie_title.innerHTML = data.title
-    sinopse.innerHTML = data.overview
-    category.innerHTML = data.genres[0].name
-    runtime.innerHTML = data.runtime + ' minutos'
-
-    const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
-    app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`
-})
-
-const moviesList = document.getElementById('movies__list')
+const moviesList = document.getElementById("movies__list");
 
 function create_movie(movieId) {
-    fetch(get_url_movie(movieId)).then(response  => response.json()).then(data => {
-        const movie = document.createElement('li')
-        const genre = `<span>${data.genres[0].name}</span>`
-        const stars = `<img src="./assets/img/Rating.svg" id="stars" alt="">`
-        const title = `<strong>${data.title}</strong>`
-        const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`
+  fetch(get_url_movie(movieId))
+    .then((response) => response.json())
+    .then((data) => {
+      const movie = document.createElement("li");
+      const genre = `<span>${data.genres[0].name}</span>`;
+      const stars = `<img src="./assets/img/Rating.svg" id="stars" alt="">`;
+      const title = `<strong>${data.title}</strong>`;
+      const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`;
 
-        movie.innerHTML = genre + stars + title
-        movie.style.backgroundImage = `linear-gradient(180deg, rgba(14, 23, 47, 0.0001) 11.72%, #0E172F 100%), url('${image}')`
+      movie.innerHTML = genre + stars + title;
+      movie.style.backgroundImage = `linear-gradient(180deg, rgba(14, 23, 47, 0.0001) 11.72%, #0E172F 100%), url('${image}')`;
+      moviesList.appendChild(movie);
 
-        moviesList.appendChild(movie)
-    })
+      movie.onclick = () => {
+        fetch(get_url_movie(movieId))
+          .then((response) => response.json())
+          .then((data) => {
+            const app = document.getElementById("app");
+
+            const movie_title = document.querySelector(".movie h2");
+            const sinopse = document.querySelector(".movie #description");
+            const category = document.querySelector(".movie span");
+
+            const runtime = document.querySelector(".rating__time p");
+
+            movie_title.innerHTML = data.title;
+            sinopse.innerHTML = data.overview;
+            category.innerHTML = data.genres[0].name;
+            runtime.innerHTML = data.runtime + ' minutos';
+
+            const image = `https://image.tmdb.org/t/p/original${data.backdrop_path}`;
+            app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`;
+          });
+      };
+    });
 }
 
 function load_list_movies() {
-    LIST_MOVIES.map(create_movie)
+  LIST_MOVIES.map(create_movie);
 }
 
-load_list_movies()
+function load_first_movie() {
+  const app = document.getElementById("app");
 
+  const movie_title = document.querySelector(".movie h2");
+  const sinopse = document.querySelector(".movie #description");
+  const category = document.querySelector(".movie span");
+
+  const runtime = document.querySelector(".rating__time p");
+
+  movie_title.innerHTML = 'Batman';
+  sinopse.innerHTML = 'Após dois anos espreitando as ruas como Batman, Bruce Wayne se encontra nas profundezas mais sombrias de Gotham City. Com poucos aliados confiáveis, o vigilante solitário se estabelece como a personificação da vingança para a população.';
+  category.innerHTML = 'Crime';
+  runtime.innerHTML = "185 minutos";
+
+  const image = 'https://image.tmdb.org/t/p/original/IYUD7rAIXzBM91TT3Z5fILUS7n.jpg';
+  app.style.backgroundImage = `linear-gradient(90.18deg, rgba(13, 22, 46, 0.7) 23.21%, rgba(13, 22, 46, 0.0001) 96.69%), url('${image}')`;
+}
+
+load_first_movie()
+
+load_list_movies();
